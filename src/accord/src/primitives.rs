8b119a5fb3398c64262559bce2ab5c3cfbab667b
Aleksandr Ushakov <all.bromles@gmail.com>
2024-12-31 16:08:16 +0300

    update primitives

--- src/accord/src/primitives.rs
+++ src/accord/src/primitives.rs
@@ -2,6 +2,30 @@ pub struct TxnId(pub u64);
 
 pub struct Ballot(pub u64);
 
-pub struct ObjectKey(pub String);
+pub struct SimpleObjectKey(pub String);
 
-pub struct Dependency(pub ObjectKey, pub TxnId);
+pub struct RangedObjectKey {
+    pub from: SimpleObjectKey,
+    pub to: SimpleObjectKey,
+}
+
+pub trait ObjectKey {
+    fn match_objects(&self) -> Vec<&SimpleObjectKey>;
+}
+
+impl ObjectKey for SimpleObjectKey {
+    fn match_objects(&self) -> Vec<&SimpleObjectKey> {
+        vec![self]
+    }
+}
+
+impl ObjectKey for RangedObjectKey {
+    fn match_objects(&self) -> Vec<&SimpleObjectKey> {
+        vec![&self.from, &self.to]
+    }
+}
+
+pub struct Dependency<T: ObjectKey> {
+    pub key: T,
+    pub txn_id: TxnId,
+}
